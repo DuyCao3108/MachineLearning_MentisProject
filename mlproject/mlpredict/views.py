5fca124278a131f091612fb63a58cc288bbe8b3e
DuyCao3108 <136797474+DuyCao3108@users.noreply.github.com>
2023-06-16 15:45:12 +0700

    Add files via upload

---
+++ mlproject/mlpredict/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import pickle as pkl
+
+from joblib import dump, load
+model = load(r"C:\Users\Windows 10\mlproject\pkmodel" + ".pickle")
+
+# def form(request):
+#     if request.method == 'POST':
+#             price = request.POST['price']
+#             perception_usefullness = request.POST['perception_usefullness']
+#             percetion_aesthetic = request.POST['percetion_aesthetic']
+#             perception_trustworthy = request.POST['perception_trustworthy']
+#             perception_relaxing = request.POST['perception_relaxing']
+#             personality_extroversion = request.POST['personality_extroversion']
+#             personality_neuroticism = request.POST['personality_neuroticism']
+#             knowledge_importance = request.POST['knowledge_importance']
+#             knowledge_otherapp = request.POST['knowledge_otherapp']
+#             y_pred = model.predict([[price, perception_usefullness, percetion_aesthetic, 
+#                                      perception_trustworthy,perception_relaxing,
+#                                      personality_extroversion,personality_neuroticism,
+#                                      knowledge_importance,knowledge_otherapp]])
+#             if y_pred[0] == 0:
+#                 y_pred = 'IS NOT Intended to pay'
+#             else:
+#                 y_pred = 'IS Intended to pay'
+#             return render(request, 'test1.html', {'result' : y_pred})
+#     return render(request, 'test1.html')
+
+def results(request):
+     if request.method == 'POST':
+            price = request.POST['price']
+            perception_usefullness = request.POST['perception_usefullness']
+            percetion_aesthetic = request.POST['percetion_aesthetic']
+            perception_trustworthy = request.POST['perception_trustworthy']
+            perception_relaxing = request.POST['perception_relaxing']
+            personality_extroversion = request.POST['personality_extroversion']
+            personality_neuroticism = request.POST['personality_neuroticism']
+            knowledge_importance = request.POST['knowledge_importance']
+            knowledge_otherapp = request.POST['knowledge_otherapp']
+            y_pred = model.predict([[price, perception_usefullness, percetion_aesthetic, 
+                                     perception_trustworthy,perception_relaxing,
+                                     personality_extroversion,personality_neuroticism,
+                                     knowledge_importance,knowledge_otherapp]])
+            if y_pred[0] == 0:
+                y_pred = 'DOES Intend to pay'
+            else:
+                y_pred = 'DOES NOT Intend to pay'
+            return render(request, 'results.html', {'result' : y_pred})
+     
+
+def form(request):
+     return render(request, 'test1.html')
